6f4fa7e5061a6192e205025f9d55823a2644e0a1
azharalsharbaji <113019567+azharalsharbaji@users.noreply.github.com>
2023-03-17 15:10:01 +0300

    add base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === "production" ? "/diamond-without-pwa/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/diamond-without-pwa/" : "/",
   // base: '/diamond-castle',
   
   plugins: [
